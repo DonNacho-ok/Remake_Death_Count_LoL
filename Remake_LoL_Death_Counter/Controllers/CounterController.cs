71e714eed33ea3fc1caccafc9712cdd15a17f1c0
Jose Ignacio Grillo <ignaciog@shifta.la>
2021-02-06 19:56:26 -0300

    Add Carpetas de arquitectura y Controller/Vista base

---
+++ Remake_LoL_Death_Counter/Controllers/CounterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Remake_LoL_Death_Counter.Controllers
+{
+    public class CounterController : Controller
+    {
+        const string RiotApiKey = "";
+
+        public ActionResult Counter()
+        {
+            return View();
+        }
+
+        public ActionResult GetTotalDeaths(string NombreInvocador, string Servidor)
+        {
+            var placeholder = 0;
+            return Json(placeholder);
+        }
+    }
+}
---
+++ Remake_LoL_Death_Counter/Models/ConterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Remake_LoL_Death_Counter.Models
+{
+    public class ConterViewModel
+    {
+        public string NombreInvocador { get; set; }
+        public string Servidor { get; set; }
+        public int CantidadMuertes { get; set; }
+    }
+}
