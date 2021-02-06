ba90f076b167ae4701f2ac379fbd7c0197ef5366
Jose Ignacio Grillo <ignaciog@shifta.la>
2021-02-06 19:27:22 -0300

    Init Repositorio
    inicio de repositorio con la creacion del proyecto base

---
+++ Remake_LoL_Death_Counter/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Remake_LoL_Death_Counter.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Remake_LoL_Death_Counter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Remake_LoL_Death_Counter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Remake_LoL_Death_Counter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
